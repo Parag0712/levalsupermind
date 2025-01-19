f3a25551b740eb902e1dde0569123b1c3377a705
SandeepSwain <sandeepswain2004@gmail.com>
2025-01-19 08:51:45 +0530

    update code

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -38,7 +38,7 @@ export const SIDE_BAR_MENU: SIDE_BAR_MENU_PROPS[] = [
   {
     label: "Appointments",
     icon: <CalIcon />,
-    path: "appointment",
+    path: "myblogs",
   },
   // {
   //   label: "Email Marketing",
